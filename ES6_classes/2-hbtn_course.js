0e1e5a82e15dbbba199ce7e396710d6a7297f6ae
Phantomwatchdog <8706@holbertonstudents.com>
2024-07-17 15:25:37 +0200

    Add TypeError

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,10 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
+      throw TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+      throw TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array');
     }
     this._name = name;
     this._length = length;
@@ -15,29 +18,32 @@ export default class HolbertonCourse {
     return this._name;
   }
 
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
     }
+    this._name = name;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
     }
+    this._length = length;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      this._students = newStudents;
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array');
     }
+    this._students = students;
   }
 }
