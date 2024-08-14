29cc090dfdc01383d1ecfc6ea30e5e44c0963ef8
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-14 12:30:53 +0200

    Test with docs

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+""" This module contains the function for task 0 """
 
 def add(a: float, b: float) -> float:
+    """ This function returns the sum of two floats """
     return a + b
