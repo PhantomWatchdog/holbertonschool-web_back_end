63ac1aa74dbb06a517cbea09cab52deec22f6c85
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-14 12:56:45 +0200

    Complete task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" This module contains the function for task 1 """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This function returns the concatenation of two strings """
+    return str1 + str2
