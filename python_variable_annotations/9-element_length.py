f3e9e2db088736ddd8556c748d0ef9497b81d539
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-14 13:47:59 +0200

    Task 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Type-annotated function element_length that takes a list
+input_list of strings and returns a list of tuples, where each tuple
+contains a string from the input list and its length.
+"""
+
+from typing import List, Tuple, Sequence, Iterable
+
+def element_length(input_list: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return list of tuples containing strings and their lengths"""
+    return [(i, len(i)) for i in input_list]
