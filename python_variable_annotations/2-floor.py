0f12bd768a162cf2842b37ec55c1add4f939ca68
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-14 13:03:06 +0200

    Fix issues

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+    
+""" This module contains the function for task 2 """
+
+import math
+
+def floor(n: float) -> int:
+    """ This function returns the floor of a float """
+    return math.floor(n)
--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -2,6 +2,7 @@
 
 """ This module contains the function for task 3 """
 
+
 def to_str(n: float) -> str:
     """ This function returns the string representation of a float """
     return str(n)
