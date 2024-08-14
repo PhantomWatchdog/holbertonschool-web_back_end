182b18aeb3e6a0b90ad5e013856a65dd6406cfeb
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-14 12:32:35 +0200

    Fix pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ This module contains the function for task 0 """
 
+
 def add(a: float, b: float) -> float:
     """ This function returns the sum of two floats """
     return a + b
