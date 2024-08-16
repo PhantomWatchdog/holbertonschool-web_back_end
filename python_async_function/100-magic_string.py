b227209c3d2676623410c889c8f06b7ff4cab5c4
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-16 09:08:12 +0200

    Move file in right folder

--- python_async_function/100-magic_string.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-def magic_string():
-    if not hasattr(magic_string, 'i'):
-        magic_string.i = 0
-    magic_string.i += 1
-    return ", ".join(["BestSchool"] * magic_string.i)
