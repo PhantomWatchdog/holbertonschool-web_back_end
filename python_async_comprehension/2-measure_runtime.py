19f1bed5e29b9acee862b8f955fca82949276f59
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-19 12:48:24 +0200

    Fix module call issue

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
-import asyncio
-from time import time
-
 """
 Import async_generator from the previous task
 """
-
+import asyncio
+import typing
+import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def measure_runtime() -> float:
+
+async def measure_runtime() -> typing.List[float]:
     """
     Measure the runtime of the async_comprehension function.
     """
-    start_time = time()
+    start_time = time.time()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    end_time = time()
+    end_time = time.time()
     return end_time - start_time
