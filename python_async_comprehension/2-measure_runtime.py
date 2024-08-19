4443d88c1480503ed7edbca61f58d33b7efd87b6
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-19 13:04:06 +0200

    Simplification of asyncio.gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,21 +4,15 @@
 Import async_generator from the previous task
 """
 import asyncio
-import typing
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> typing.List[float]:
+async def measure_runtime() -> float:
     """
     Measure the runtime of the async_comprehension function.
     """
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
     return end_time - start_time
