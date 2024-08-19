563ae7ef93618bdd54b6fcb7dca1697a95de0245
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-19 10:24:48 +0200

    Update code

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,13 +2,14 @@
 
 import asyncio
 import random
+import typing
 
 """ Asynchronous generator that yields random
 floating-point numbers between 0 and 10.
 """
 
 
-async def async_generator() -> int:
+async def async_generator() -> typing.Generator[float, None, None]:
     """
     Asynchronous generator that yields random
     floating-point numbers between 0 and 10.
