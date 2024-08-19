e3f63796115bc3d1c6fa2c77e0fda8b9fdecc589
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-19 09:43:17 +0200

    Add doc

--- python-async-comprehension/0-async_generator.py
+++ python-async-comprehension/0-async_generator.py
@@ -4,8 +4,18 @@ import asyncio
 from typing import List
 import random
 
+""" Asynchronous generator that yields random
+floating-point numbers between 0 and 10.
+"""
+
 
 async def async_generator() -> List[float, None]:
+    """
+    Asynchronous generator that yields random floating-point numbers between 0 and 10.
+
+    Returns:
+        List[float, None]: A list of floating-point numbers or None.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
