7fed3e25ee7a697829c407dd4aaa23cd50163003
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-22 19:24:38 +0200

    Add shebang

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+""" Lists all documents in a collection """
 def list_all(mongo_collection):
     """ Lists all documents in a collection """
     return list(mongo_collection.find())
