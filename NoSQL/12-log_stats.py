200cbc8511f0b80d8914d377e1b0229946388b04
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-23 15:54:31 +0200

    Test new version

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
-""" 12-log_stats """
-
+"""Python script that provides statistics
+about Nginx logs stored in MongoDB."""
 from pymongo import MongoClient
 
-def log_stats():
-    """Provides stats for Nginx logs"""
+
+def main():
+    """fonction that provides statistics"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx = client.logs.nginx
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: collection.count_documents(
+        {"method": method}) for method in methods}
 
-    print("{} logs".format(nginx.count_documents({})))
+    status_check_count = collection.count_documents(
+        {"method": "GET", "path": "/status"})
+
+    print(f"{total_logs} logs")
     print("Methods:")
-
     for method in methods:
-        print("\tmethod {}: {}".format(method, nginx.count_documents
-                                       ({'method': method})))
-
-    print("{} status check".format(nginx.count_documents
-                                   ({'method': 'GET', 'path': '/status'})))
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check_count} status check")
 
 
 if __name__ == "__main__":
-    log_stats()
+    main()
