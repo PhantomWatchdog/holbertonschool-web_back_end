78cc53d31c428c62e2511406c4486d7d2aeb5e11
Phantomwatchdog <8706@holbertonstudents.com>
2024-09-20 18:32:33 +0200

    Testing new version for task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,19 +4,16 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (_, res) => {
+  res.set('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (_, res) => {
+  res.set('Content-Type', 'text/plain');
   countStudents(process.argv[2])
     .then((data) => {
       res.write('This is the list of our students\n');
       res.write(data);
-      res.end();
-    })
-    .catch((error) => {
-      res.write('This is the list of our students\n');
-      res.write(error.message);
       res.end();
     });
 });
